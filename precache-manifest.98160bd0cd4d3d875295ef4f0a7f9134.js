29d9623e082d96b640d7391b2d95b0e1fe1cb32c
Ramon Medeiros <ramon.medeiros@carla.se>
2023-05-02 22:13:02 +0200

    Updates

---
+++ precache-manifest.98160bd0cd4d3d875295ef4f0a7f9134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d84b3f036c9f03ea212",
+    "url": "/static/js/main.4d84b3f0.chunk.js"
+  },
+  {
+    "revision": "f9a280cce4a84bd8f31a",
+    "url": "/static/js/1.f9a280cc.chunk.js"
+  },
+  {
+    "revision": "997579dda3c8051b3142",
+    "url": "/static/js/2.997579dd.chunk.js"
+  },
+  {
+    "revision": "148dedf133217d189ab6",
+    "url": "/static/js/runtime~main.148dedf1.js"
+  },
+  {
+    "revision": "0d464a02d07dfa4bc5f7aa50ed0a55b1",
+    "url": "/index.html"
+  }
+];
